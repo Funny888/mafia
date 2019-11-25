4f7770c8ae20671cd2ef21ad00023905c0bbb507
Ярослав <yaroslav888@list.ru>
2019-11-25 20:05:48 +0300

    fix clear database

--- app/src/main/java/com/example/mafia/utils/ClickUtils.java
+++ app/src/main/java/com/example/mafia/utils/ClickUtils.java
@@ -1,6 +1,7 @@
 package com.example.mafia.utils;
 
 import android.content.Context;
+import android.os.SystemClock;
 import android.util.Log;
 
 import com.example.mafia.databases.RepositoryDB;
@@ -11,10 +12,10 @@ import java.util.TimerTask;
 public class ClickUtils {
     private int mTab;
     private Context mContext;
-    private Timer mTimer;
     private TimerTask mTamerTask;
     private Runnable myRunnable;
     private Thread myThread;
+    private boolean mIsStop = false;
 
     public ClickUtils(Context context) {
         mContext = context;
@@ -22,29 +23,25 @@ public class ClickUtils {
             @Override
             public void run() {
                 RepositoryDB.getInstanse(mContext).deleteStatistic();
+                mIsStop = false;
             }
         };
 
-        myRunnable = new Runnable() {
-            @Override
-            public void run() {
-                int i = 1;
-                while (i < 6) {
-                    try {
-                        Thread.sleep(1000);
-                        if (mTab == 5) {
-                            Log.d("ClickUtils", "tabs: " + mTab);
-                            mTamerTask.run();
-                        } else {
-                            if (i == 5) {
-                                mTab = 0;
-                            }
-                            i++;
-                            Log.d("ClickUtils", "i: " + i);
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        myRunnable = () -> {
+            int i = 1;
+            while (i < 6) {
+                try {
+                    Thread.sleep(1000);
+                    if (mTab >= 5) {
+                        mTamerTask.run();
+                        return;
+                    } else if (i == 5) {
+                        mTab = 0;
+                        mIsStop = false;
                     }
+                    i++;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         };
@@ -52,21 +49,11 @@ public class ClickUtils {
 
     public void incrementTab() {
         mTab++;
-        fiveTab();
-    }
-
-    private boolean fiveTab() {
-        if (myThread == null){myThread = new Thread(myRunnable);}
-        Log.d("fiveTab", "isAlive: " + myThread.isAlive());
-        if (myThread.isAlive()) {
-            Log.d("isAlive", "fiveTab: " + true);
-            return true;
-        } else {
-            Log.d("isAlive", "fiveTab: " + false);
+        if (!mIsStop) {
+            mIsStop = true;
+            myThread = new Thread(myRunnable);
             myThread.start();
-            return false;
         }
     }
-
 
 }
